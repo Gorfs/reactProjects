71d17d9fde495338eadb5d9a95454d3fa66c8f7c
Gorfgorf23 <archiebeales@gmail.com>
2022-04-17 11:04:47 +0200

    react is not good for my portfolio, going back to normal html and css

---
+++ portfolio/src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Archie Beales</h1>
+      <p>I'm british lol</p>
+      <uo>
+        <li>The states</li>
+        <li>Japan</li>
+        <li>ye mum's house</li>
+      </uo>
+    </div>
+  )
+}
+export default MyInfo
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,18 +1,6 @@
 import React from "react"
 import reactDom from "react-dom"
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Archie Beales</h1>
-      <p>I'm british lol</p>
-      <uo>
-        <li>The states</li>
-        <li>Japan</li>
-        <li>ye mum's house</li>
-      </uo>
-    </div>
-  )
-}
+import MyInfo from "./components/MyInfo"
 
 reactDom.render(<MyInfo />, document.getElementById("root"))
